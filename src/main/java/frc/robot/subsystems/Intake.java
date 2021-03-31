bfbe77fe15f4abe97977a497b2a2098b7aa4768c
goateefrench <goateefrench@gmail.com>
2021-03-31 15:20:19 -0400

    Libraries REV, Phoenix

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,11 +5,34 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+import com.revrobotics.*;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 public class Intake extends SubsystemBase {
+  
+  //Intake has One NEO550
+  private final CANSparkMax intake = new CANSparkMax(Constants.INTAKE, CANSparkMaxLowLevel.MotorType.kBrushless);
+  
+  private double intakeSet;
+  
+  
   /** Creates a new Intake. */
-  public Intake() {}
-
+  public Intake() {
+    intake.setIdleMode(IdleMode.kBrake); //set how the motor behaves Idle
+    intake.setSmartCurrentLimit(30); //Prevents Explosion
+    this.intakeSet = 0; // Sets the speed to 0
+  }
+  /**Sets the speed of the intake Motor */
+  public void setIntake (double speed) {
+    this.intakeSet = speed;
+  }
+  /**Stops the Intake motor */
+  public void stopIntake() {
+    intakeSet = 0;
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
