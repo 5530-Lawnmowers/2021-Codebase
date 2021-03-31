f57bf5a9f92fbb513b288c94f990832e4791031a
goateefrench <goateefrench@gmail.com>
2021-03-31 16:52:43 -0400

    Added HoodIn, HoodOut Commands. Hood Subsystem added basic methods

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -5,11 +5,29 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+import com.revrobotics.*;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 public class Hood extends SubsystemBase {
   /** Creates a new Hood. */
-  public Hood() {}
+  private final CANSparkMax hood = new CANSparkMax(Constants.HOOD, CANSparkMaxLowLevel.MotorType.kBrushed);
+  
+  private double hoodSet;
 
+  public Hood() {
+    hood.setIdleMode(IdleMode.kBrake); //set how the motor behaves Idle
+    hood.setSmartCurrentLimit(30); //Prevents Explosion
+    this.hoodSet = 0; // Sets the speed to 0
+  }
+  public void setHood(double speed) {
+    hood.set(speed);
+  }
+  public void stopHood() {
+    hood.set(0);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
