c4f26718d2d71a71003b67eef640098ae6d21345
goateefrench <goateefrench@gmail.com>
2021-04-07 22:12:13 -0400

    Smartshoot Fixed Stuff
    Indentation Error. Some Logic stuff. Syntax

--- src/main/java/frc/robot/commands/SmartShoot.java
+++ src/main/java/frc/robot/commands/SmartShoot.java
@@ -7,15 +7,14 @@ public class SmartShoot extends CommandBase{
     private Shooter shooter;
     private Feed feed;
     private double accel = 1.0;
-    private double backDrive = -0.2; 
+    private double shoot = 0.9;
     private double TARGET = 4500;
     private double THRESHOLD = 4450;
-    private double spindexFeed = 1.0;
-    private double gateWheelFeed = 0.75;
-    private double shoot = 0.9;
+    private double spindexFeed = .5;
+    private double gateWheelFeed = 1;
+
     public SmartShoot(Shooter shooter, Feed feed) {
-        addRequirements(shooter);
-        addRequirements(feed);
+        addRequirements(shooter, feed);
         this.shooter = shooter;
         this.feed = feed;
     }
@@ -23,21 +22,25 @@ public class SmartShoot extends CommandBase{
 
     }
     public void execute() {
-        if (shooter.getVelocity() < THRESHOLD) {
-            shooter.setShooter(accel);
-            if (feed.getBreakbeam()) {
-              feed.setGateWheel(backDrive);
-              feed.setSpindex(spindexFeed);
-            } else {
-              feed.stopGateWheel();
-            }
-          } else if (shooter.getVelocity() < TARGET) {
-            shooter.setShooter(shoot);
-          } else {
-            shooter.setShooter(shoot);
-            feed.setGateWheel(gateWheelFeed);
-          }
-
+        
+      if (shooter.getVelocity() < THRESHOLD) { //Flywheel < 4500
+          shooter.setShooter(accel);//Sets Shooter voltage to 1.0 
+         
+        if (feed.getBreakbeam()) { // If the breakbeam returns true(clear)
+          feed.setGateWheel(gateWheelFeed); //feeds the balls up
+          feed.setSpindex(spindexFeed); // feeds the balls toward the gatewheel
+        } else { //If the breakbeam returns false
+          feed.stopGateWheel(); //stops feeding the balls
+          feed.stopSpindex();
+        }
+          
+      } else if (shooter.getVelocity() < TARGET) { //4450 <= Flywheel Speed < Target
+        shooter.setShooter(shoot); //Set flyhweel to 0.9 output
+      } else {//Flywheel >= 4500
+        shooter.setShooter(shoot); //Sets flywheel to 0.9 output
+        feed.setGateWheel(gateWheelFeed); //Feeds cells toward the shooter
+        feed.setSpindex(spindexFeed);
+      }
     }
     public void end(boolean interrupted) {
         shooter.stopShooter();
